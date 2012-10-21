34618da4566dbb9bf048a260ee1a9c9f5617679f
Kostas Antonopoulos <kostas@devpro.gr>
2012-10-21 22:55:51 +0300

    Added .gitignore. Added Files to start

---
+++ DPCustomTabBarController/DPCustomTabBarController/DPCustomTabBarController.m
@@ -0,0 +1,38 @@
+//
+//  DPCustomTabBarController.m
+//  DPCustomTabBarController
+//
+//  Created by Kostas Antonopoulos on 10/21/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import "DPCustomTabBarController.h"
+
+@interface DPCustomTabBarController ()
+
+@end
+
+@implementation DPCustomTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
