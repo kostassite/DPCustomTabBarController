a1dfb39e86f5407125191422c55a7bad07a70480
Kostas Antonopoulos <kostas@devpro.gr>
2012-10-21 22:42:33 +0300

    Initial Commit

---
+++ DPCustomTabBarController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DPCustomTabBarController
+//
+//  Created by Kostas Antonopoulos on 10/21/12.
+//  Copyright (c) 2012 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
